ac5ef93e29980f70c717704b809904aef6f294da
mrashidi <m.rashidi@sadad.co.ir>
2020-02-16 18:24:15 +0330

    4th commit

---
+++ src/main/java/com/in28minutes/database/databasedemo/jdbc/PersonJdbcDao.java
@@ -0,0 +1,21 @@
+package com.in28minutes.database.databasedemo.jdbc;
+
+import com.in28minutes.database.databasedemo.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PersonJdbcDao {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Person>findAll(){
+
+        jdbcTemplate.query("select * frpm person",new BeanPropertyRowMapper<>(Person.class));
+        return null;
+    }
+}
